0ae7e4ebc333be487b5f8ecd8d9771042b330c97
makesh naidu <32321267+makeshnaidu2011@users.noreply.github.com>
2020-09-22 21:25:22 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase"
+const firebaseConfig = {
+   //your config
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider();
+export { auth, provider }
+export default db;
